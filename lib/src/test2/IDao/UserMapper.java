e4d380eecd9fed55b739f6415e6c6a4303d57215
jueye3 <wangjs@channelsoft.com>
2018-05-28 10:58:32 +0800

    first commit

---
+++ lib/src/test2/IDao/UserMapper.java
@@ -0,0 +1,17 @@
+package test2.IDao;
+
+import test2.domain.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer uid);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer uid);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
